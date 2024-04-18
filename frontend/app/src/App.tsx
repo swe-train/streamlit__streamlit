59b696ac3e75fca74f56de1ba61864f04c4c66db
lukasmasuch <lukas.masuch@gmail.com>
2024-04-18 18:59:31 +0200

    Apply additional fixes

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -560,7 +560,7 @@ export class App extends PureComponent<Props, State> {
 
     if (newState === ConnectionState.CONNECTED) {
       logMessage("Reconnected to server; requesting a script run")
-      this.widgetMgr.sendUpdateWidgetsMessage()
+      this.widgetMgr.sendUpdateWidgetsMessage(undefined)
       this.setState({ dialog: null })
     } else {
       setCookie("_streamlit_xsrf", "")
@@ -1307,7 +1307,7 @@ export class App extends PureComponent<Props, State> {
       this.saveSettings({ ...this.state.userSettings, runOnSave: true })
     }
 
-    this.widgetMgr.sendUpdateWidgetsMessage()
+    this.widgetMgr.sendUpdateWidgetsMessage(undefined)
   }
 
   sendLoadGitInfoBackMsg = (): void => {
